1eb8387f4b8afc2d6f96eb420ab88a1b0f7c457f
Daniel Alderete <daniel.alderetecillero@telefonica.com>
2021-05-22 11:16:45 +0200

    Added global service file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GLOBAL } from './services/global';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular 4 - Products';
+  public title: string = 'Angular 4 - Products';
+  public headerColour: string;
+
+  constructor() {
+    this.headerColour = GLOBAL.headerColour;
+  }
 }
